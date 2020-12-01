30594924b30e55ba79825cf9e469e82152c14478
ethanparab <59212466+ethanparab@users.noreply.github.com>
2020-12-01 10:18:21 -0600

    added print statement

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace visstd_test
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("This is a test for Github");
         }
     }
 }
